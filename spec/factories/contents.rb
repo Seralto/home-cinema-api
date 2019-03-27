0590c8e43e56b0f51b73a39f442440edca6fe237
Sérgio Toledo <contato@sergiotoledo.com.br>
2019-03-27 15:10:38 -0300

    Refactor contents spec

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -3,21 +3,21 @@ FactoryBot.define do
     title { Faker::Lorem.word }
     plot { Faker::Lorem.paragraph }
     type { %w(Movie Season).sample }
-  end
 
-  factory :invalid_content, class: 'Content' do
-    title { nil }
-  end
+    factory :invalid_content do
+      title { nil }
+    end
 
-  factory :invalid_content_type, class: 'Content' do
-    type { nil }
-  end
+    factory :invalid_content_type do
+      type { nil }
+    end
 
-  factory :movie, parent: :content, class: 'Movie' do
-    type { 'Movie' }
-  end
+    factory :movie, class: 'Movie' do
+      type { 'Movie' }
+    end
 
-  factory :season, parent: :content, class: 'Season' do
-    type { 'Season' }
+    factory :season, class: 'Season' do
+      type { 'Season' }
+    end
   end
 end
