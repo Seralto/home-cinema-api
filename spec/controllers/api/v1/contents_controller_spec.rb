f805014a98872206072daf1d0e70ffa5814d33b2
Sérgio Toledo <contato@sergiotoledo.com.br>
2019-03-28 10:27:28 -0300

    Create ContentsController specs

--- spec/controllers/api/v1/contents_controller_spec.rb
+++ spec/controllers/api/v1/contents_controller_spec.rb
@@ -3,9 +3,47 @@ require 'rails_helper'
 RSpec.describe Api::V1::ContentsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    let!(:movie1) { create(:movie) }
+    let!(:movie2) { create(:movie) }
+    let!(:season1) { create(:season) }
+    let!(:season2) { create(:season) }
+    let!(:episode) { create(:episode, season: season2) }
+
+    context "/contents" do
+      it "returns all contents ordered by creation date" do
+        get :index
+        json = JSON.parse(response.body)
+        expect(response).to be_success
+        expect(json.size).to eq(4)
+      end
+    end
+
+    context "/movies" do
+      it "returns all movies ordered by creation date" do
+        get :index, params: { by_type: 'Movie' }
+        json = JSON.parse(response.body)
+        expect(response).to be_success
+        expect(json.size).to eq(2)
+        expect(json.last['id']).to eq(movie2.id)
+      end
+    end
+
+    context "/seasons" do
+      it "returns all seasons ordered by creation date" do
+        get :index, params: { by_type: 'Season' }
+        json = JSON.parse(response.body)
+        expect(response).to be_success
+        expect(json.size).to eq(2)
+        expect(json.last['id']).to eq(season2.id)
+      end
+
+      it "returns all seasons with their respective episodes" do
+        get :index, params: { by_type: 'Season' }
+        json = JSON.parse(response.body)
+        expect(response).to be_success
+        expect(json.first['episodes'].size).to eq(0)
+        expect(json.last['episodes'].size).to eq(1)
+      end
     end
   end
 
