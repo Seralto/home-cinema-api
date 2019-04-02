5778ad507b2f562aaa3cc37256b958ed44ce719d
Seralto <stoledo13@gmail.com>
2019-04-02 13:39:22 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-5.times do
+50.times do
   movie = Movie.create(title: Faker::Book.title, plot: Faker::Quote.matz)
   PurchaseOption.create(price: Faker::Number.decimal(1, 2), quality: 'HD', content: movie)
   PurchaseOption.create(price: Faker::Number.decimal(1, 2), quality: 'SD', content: movie)
 end
 
-2.times do
+10.times do
   season = Season.create(title: Faker::Book.title, plot: Faker::Quote.matz)
   PurchaseOption.create(price: Faker::Number.decimal(1, 2), quality: 'HD', content: season)
   PurchaseOption.create(price: Faker::Number.decimal(1, 2), quality: 'SD', content: season)
 
-  10.times { |i| season.episodes.create(title: Faker::Book.title, plot: Faker::Quote.famous_last_words, number: i + 1) }
+  5.times { |i| season.episodes.create(title: Faker::Book.title, plot: Faker::Quote.famous_last_words, number: i + 1) }
 end
 
 user = User.create(email: Faker::Internet.email)
